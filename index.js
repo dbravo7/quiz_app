bbd453b94b2ca63d0bf21cfe48925e994721f06d
David <dbravo7@yahoo.com>
2019-06-06 21:44:14 -0400

    Implemented function to handle submit button; still debugging functions called within it

--- index.js
+++ index.js
@@ -64,6 +64,7 @@ function answerTemplate(result) {
         <input type="radio" name="answer" checked>
         <span>${questions[i].answer}</span>
       </div>
+      <a href=${questions[i].link}> 
       <img src="${questions[i].image}" alt="book cover">
     </fieldset>
     <div class=" button_container">
@@ -116,14 +117,14 @@ function increaseScore() {
 function handleStartButton() {
   $('main').on('click', '.start_button', event => {
     event.preventDefault();
-    $('.main_page').remove();
+    $('main').remove();
     renderQuestionPage();
   });
 }
 
 // on submit, check if question is correct and pass as argument to answerTemplate
 function handleSubmitButton() {
-  $('form').on('submit', function (event) {
+  $('body').on('submit', 'form', function (event) {
     event.preventDefault();
     const answer = $('input:checked').siblings('span');
     const result = checkAnswer(answer);
@@ -163,6 +164,7 @@ function populateQuestions() {
       option4: 'All of the above',
       answer: 'The Count of Monte Cristo',
       image: 'images/Countofmonte.jpg',
+      link: 'https://www.amazon.com/Count-Monte-Cristo-Penguin-Classics/dp/0140449264/ref=sr_1_2?crid=KDUYMJRUKQ21&keywords=count+of+monte+cristo+book&qid=1559871329&s=gateway&sprefix=count+of+%2Caps%2C130&sr=8-2'
     },
     {
       question: 'Who wrote the famous Spanish novel Don Quixote?',
@@ -171,7 +173,8 @@ function populateQuestions() {
       option3: 'Miguel de Cervantes',
       option4: 'None of the above',
       answer: 'Miguel de Cervantes',
-      image: 'images/Don-Quijote-book.jpg'
+      image: 'images/Don-Quijote-book.jpg',
+      link: 'https://www.amazon.com/Quixote-Penguin-Classics-Cervantes-Saavedra/dp/0142437239/ref=sr_1_3?crid=SY6C44NXR5F1&keywords=don+quixote&qid=1559871297&s=gateway&sprefix=don+qui%2Caps%2C129&sr=8-3'
     },
     {
       question: 'In the book Twenty Years After, which well known characters have major roles in the plot?',
@@ -180,7 +183,8 @@ function populateQuestions() {
       option3: 'Sherlock Holmes and Watson',
       option4: 'Don Quixote and Sancho Panza',
       answer: 'The three Musketeers',
-      image: 'images/Twent_years_after.jpg'
+      image: 'images/Twent_years_after.jpg',
+      link: 'https://www.amazon.com/Twenty-Years-Oxford-Worlds-Classics/dp/0199537267/ref=sr_1_1?keywords=Twenty+Years+after&qid=1559871243&s=gateway&sr=8-1'
     },
     {
       question: 'Which European general is the antagonist in the book War and Peace?',
@@ -189,7 +193,8 @@ function populateQuestions() {
       option3: 'Hannibal',
       option4: 'Napoléon Bonaparte',
       answer: 'Napoléon Bonaparte',
-      image: 'images/war_and_peace.jpg'
+      image: 'images/war_and_peace.jpg',
+      link: 'https://www.amazon.com/War-Peace-Vintage-Classics-Tolstoy/dp/1400079985/ref=sr_1_1_sspa?keywords=war+and+peace&qid=1559871209&s=gateway&sr=8-1-spons&psc=1'
     },
     {
       question: 'In the book, The Brothers Karamazov, which of the following is true?',
@@ -198,7 +203,8 @@ function populateQuestions() {
       option3: 'One brother marries and then leaves his spouse to join a monastery',
       option4: 'None of the above',
       answer: 'None of the above',
-      image: 'images/the-brothers-karamazon.jpg'
+      image: 'images/the-brothers-karamazon.jpg',
+      link: 'https://www.amazon.com/Brothers-Karamazov-Novel-Parts-Epilogue/dp/0140449248/ref=sr_1_3?crid=2UR2WVFRHB75S&keywords=brothers+karamazov&qid=1559871168&s=gateway&sprefix=brothers+kara%2Caps%2C123&sr=8-3'
     },
     {
       question: 'Who wrote East of Eden?',
@@ -207,7 +213,8 @@ function populateQuestions() {
       option3: 'John Steinbeck',
       option4: 'Mark Twain',
       answer: 'John Steinbeck',
-      image: 'images/east_of_eden.jpeg'
+      image: 'images/east_of_eden.jpeg',
+      link: 'https://www.amazon.com/East-Penguin-Twentieth-Century-Classics/dp/0140186395/ref=sr_1_1?keywords=East+of+Eden&qid=1559871125&s=gateway&sr=8-1'
     },
     {
       question: "In which of the following books did a supporting character have the nickname 'tea cake'?",
@@ -217,6 +224,7 @@ function populateQuestions() {
       option4: 'Their Eyes Were Watching God',
       answer: 'Their Eyes Were Watching God',
       image: 'images/eyes_watching_god.jpg',
+      link: 'https://www.amazon.com/Their-Eyes-Were-Watching-God/dp/0061120065/ref=sr_1_1?crid=238592YHWKIF7&keywords=their+eyes+were+watching+god+zora+neale+hurston&qid=1559871098&s=gateway&sprefix=Their+eye%2Caps%2C125&sr=8-1'
     },
     {
       question: "John Milton's Paradise Regained is a dramatic retelling of which Bible story?",
@@ -225,7 +233,8 @@ function populateQuestions() {
       option3: "Israel's exile to Babylon",
       option4: 'Adam and Eve in the Garden of Eden',
       answer: "Jesus' temptation in the wilderness",
-      image: 'images/paradise-regained.jpg'
+      image: 'images/paradise-regained.jpg',
+      link: 'https://www.amazon.com/Paradise-Regained-John-Milton/dp/1978337027/ref=sr_1_2?keywords=paradise+regained&qid=1559871061&s=gateway&sr=8-2'
     },
     {
       question: 'The book Animal Farm is ____.',
@@ -234,7 +243,8 @@ function populateQuestions() {
       option3: 'a political allegory',
       option4: 'about child labor in the agricultural industry',
       answer: 'a political allegory',
-      image: 'images/animal_farm.jpg'
+      image: 'images/animal_farm.jpg',
+      link: 'https://www.amazon.com/Animal-Farm-George-Orwell/dp/0451526341/ref=sr_1_2?keywords=animal+farm&qid=1559871021&s=gateway&sr=8-2'
     },
     {
       question: 'The Illiad tells a story that takes place a little over a ____.',
@@ -243,7 +253,8 @@ function populateQuestions() {
       option3: 'month',
       option4: 'year',
       answer: 'month',
-      image: 'images/illiad.jpg'
+      image: 'images/illiad.jpg',
+      link: 'https://www.amazon.com/Iliad-Homer/dp/0140275363/ref=sr_1_2?keywords=the+illiad&qid=1559870981&s=gateway&sr=8-2'
     }]);
   return Q_A;
 }
